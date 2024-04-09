d230556b9883591c8c18f45fdac3ffdc265e0a9d
Michael Nestler <108884670+michael-nestler@users.noreply.github.com>
2024-04-09 22:44:02 +0200

    Add factorial demo

---
+++ bcpl/test-programs/factorial.spec.ts
@@ -0,0 +1,20 @@
+import { expect, test } from "bun:test";
+import { readFile } from "node:fs/promises";
+import { resolve } from "node:path";
+import { parseCode } from "../parser/parser";
+import { Program } from "../program";
+
+test("factorial", async () => {
+  const testScript = await readFile(resolve(import.meta.dir, "./factorial.ocode"), { encoding: "utf8" });
+  const commands = parseCode(testScript);
+
+  const program = new Program();
+  program.commands = commands;
+  commands.forEach((command, index) => {
+    if (command.operation === "LAB") {
+      program.labels.set(command.arguments[0], index);
+    }
+  });
+  while (program.next());
+  expect(program.environment.topValue()).toBe(5040);
+});
